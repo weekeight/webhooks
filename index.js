78e0f04a59d6440f1e69b45cd4003ea58ed9bb16
秋知 <benfchen.cf@alibaba-inc.com>
2014-10-21 10:45:17 +0800

    simplily done

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ var childProcess = require('child_process');
 
 var app = express();
 
-app.use(function(req, res, next){
-	console.log(req.body);
-});
-app.post(bodyParser.json());
-app.use(function(req, res, next){
-	console.log(req.body);
-});
+app.use(bodyParser.json());
+
 app.post('/deploy', function(req, res){
-	console.log(req);
-	res.status(200).send('Hello World!');
+	res.status(200).send('get deploy message, I will do it...');
+	var body = req.body,
+		name = body.repository.name,
+		branch = body.ref.split('/')[2];
+	if(name === 'docs.kissyui.com' && branch === '5.0'){
+		childProcess.exec('cd /alidata/www/docs.kissyui.com/kissyteam.github.com/5.0;git pull');
+		console.log('deploy task done...');
+	}
 });
 
 var server = app.listen(3000, function(req, res){
 	var host = server.address().address,
 		port = server.address().port;
 
-	console.log('Example app listening at http://%s:%s', host, port);
+	console.log('listen on port:' + port + ', when I get deploy message, I will do it...');
 });
