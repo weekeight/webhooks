72cf68515615837c832a69454d1febe000202899
秋知 <benfchen.cf@alibaba-inc.com>
2014-10-21 10:13:43 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var childProcess = require('child_process');
+
 var app = express();
 
-app.get('/payload', function(req, res){
+app.use(function(req, res, next){
+	console.log(req.body);
+});
+app.post(bodyParser.json());
+app.use(function(req, res, next){
+	console.log(req.body);
+});
+app.post('/deploy', function(req, res){
 	console.log(req);
-	res.send('Hello World!');
+	res.status(200).send('Hello World!');
 });
 
 var server = app.listen(3000, function(req, res){
@@ -11,4 +21,4 @@ var server = app.listen(3000, function(req, res){
 		port = server.address().port;
 
 	console.log('Example app listening at http://%s:%s', host, port);
-})
+});
